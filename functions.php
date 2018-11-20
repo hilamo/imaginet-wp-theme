f97219a935d0451f3d0ccd59551c7763f90b5c6d
Hila Moalem <hila.moalem72@gmail.com>
2018-11-20 11:53:31 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -36,8 +36,8 @@ if (function_exists('add_theme_support')){
     // Add Thumbnail Theme Support
     add_theme_support('post-thumbnails');
     // override media setting - Image sizes
-    add_image_size('small', 150, '', true); // Small Thumbnail
-    add_image_size('medium', 300, '', true); // Medium Thumbnail
+    add_image_size('small', 300, '', true); // Small Thumbnail
+    add_image_size('medium', 640, '', true); // Medium Thumbnail
     add_image_size('large', 1024, '', true); // Large Thumbnail
     // Enables post and comment RSS feed links to head
     add_theme_support('automatic-feed-links');
@@ -182,4 +182,23 @@ function hebrew_files_prevent($file) {
 	  $file['error'] = 'נא לא להעלות קבצים עם שמות בעברית!';
 	}
     return $file;
+}
+
+/**
+ * Responsive Image Helper Function
+ * @param string $image_id the id of the image (from ACF or similar)
+ * @param string $image_size the size of the thumbnail image or custom image size
+ * @param string $max_width the max width this image will be shown to build the sizes attribute
+ */
+function print_responsive_image_attr($image_id, $image_size, $max_width){
+	// check the image ID is not blank
+	if($image_id != '') {
+		// set the default src image size
+		$image_src = wp_get_attachment_image_url( $image_id, $image_size );
+		// set the srcset with various image sizes
+		$image_srcset = wp_get_attachment_image_srcset( $image_id, $image_size );
+		// generate the markup for the responsive image
+		echo 'src="'.$image_src.'" srcset="'.$image_srcset.'" sizes="(max-width: '.$max_width.') 100vw, '.$max_width.'"';
+
+	}
 }
